518a4aeb688dcdd074a62e79786cd7fb6fda18a3
devops19 <bhashithagedela@gmail.com>
2024-03-05 13:36:24 +0530

    adding shell scripting topics

--- 02-print.sh
+++ 02-print.sh
@@ -1,5 +1,5 @@
 echo hello world
-color syntax : echo -e "\e[COLm MESSAGE\e[0m"
+color syntax : echo -e "\e[COLm MESSAGE\e[Om"
 #echo -e : -e is used to allow colors
 #Double quotes are mandatory, Single quotes can be used but not preferred
 # \e[COLm -> COL is a color code we need to provide
