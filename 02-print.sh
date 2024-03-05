cfd36069167be1d5ae416533e524838a529190b7
devops19 <bhashithagedela@gmail.com>
2024-03-05 13:23:21 +0530

    adding shell scripting topics

--- 02-print.sh
+++ 02-print.sh
@@ -1,1 +1,17 @@
 echo hello world
+Color syntax : echo -e "\e[COLm Message\e[Om"
+#echo -e: -e is used to allow colors
+#Double quotes are mandatory, Single quotes can be used but not preferred
+# \e[COLm -> COL is a color code we need to provide
+ #RED       - 31
+ #GREEN     - 32
+ #YELLOW    - 33
+ #BLUE      - 34
+ #MAGENTA   - 35
+ #CYAN      - 36
+#\e[Om -> This is to disable the enabled color
+
+
+echo -e "\e[COL31 This text is in RED color\e[Om"
+echo -e "\e[COL33 This text is in YELLOW color\e[Om"
+
