68359ad395e33a9769aaad03133fdaf79f1c462e
devops19 <bhashithagedela@gmail.com>
2024-03-16 13:58:12 +0530

    shell topics

---
+++ 04-loops.sh
@@ -0,0 +1,13 @@
+#while & for loop
+
+#choose while loop if you are using expression
+#choose for loop if you having inputs
+
+#loop is going to execute the same set of commands again and again in iterative way
+
+a=10
+while [ "$a" -gt 0 ]; do
+  echo hello world
+  a=$(($a-1))
+  sleep 1
+done
